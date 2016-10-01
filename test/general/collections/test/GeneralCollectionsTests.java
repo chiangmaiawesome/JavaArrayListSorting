fe984153b5712729749bc01cd4fc14da61d5bc18
GitUser <gituser@example.com>
2016-10-01 19:58:42 +0700

    Compare array list

--- test/general/collections/test/GeneralCollectionsTests.java
+++ test/general/collections/test/GeneralCollectionsTests.java
@@ -13,33 +13,63 @@ import org.junit.*;
  * @author acer
  */
 public class GeneralCollectionsTests {
-    
+
     public GeneralCollectionsTests() {
     }
-    
+
     @Test
     public void combineArrayLists() {
         //First ArrayList
-        ArrayList<String> arraylist1=new ArrayList<>();
+        ArrayList<String> arraylist1 = new ArrayList<>();
         arraylist1.add("AL1: E1");
         arraylist1.add("AL1: E2");
         arraylist1.add("AL1: E3");
 
         //Second ArrayList
-        ArrayList<String> arraylist2=new ArrayList<>();
+        ArrayList<String> arraylist2 = new ArrayList<>();
         arraylist2.add("AL2: E1");
         arraylist2.add("AL2: E2");
         arraylist2.add("AL2: E3");
 
         //New ArrayList
-        ArrayList<String> al= new ArrayList<>();
+        ArrayList<String> al = new ArrayList<>();
         al.addAll(arraylist1);
         al.addAll(arraylist2);
 
         //Displaying elements of the joined ArrayList
         al.stream().forEach((temp) -> {
             System.out.println(temp);
-        });        
+        });
     }
-    
+
+    @Test
+    public void compareArrayList() {
+        ArrayList<String> al1 = new ArrayList<>();
+        al1.add("hi");
+        al1.add("How are you");
+        al1.add("Good Morning");
+        al1.add("bye");
+        al1.add("Good night");
+
+        ArrayList<String> al2 = new ArrayList<>();
+        al2.add("Howdy");
+        al2.add("Good Evening");
+        al2.add("bye");
+        al2.add("Good night");
+
+        //Storing the comparison output in ArrayList<String>
+        ArrayList<String> al3 = new ArrayList<>();
+        al1.stream().forEach((temp)-> {
+            al3.add(al2.contains(temp) ? "Yes" : "No");
+        });
+        System.out.println(al3);
+
+        //Storing the comparison output in ArrayList<Integer>
+        ArrayList<Integer> al4 = new ArrayList<>();
+        al1.stream().forEach((temp2) -> {
+            al4.add(al2.contains(temp2) ? 1 : 0);
+        });
+        System.out.println(al4);
+    }
+
 }
